b828f42c3ee9e344e55b2e33f8b34259b9495a17
Mirshod Mirjonov <mirshod.mirjonov@tangoanalytics.com>
2025-05-30 13:11:22 +0500

    some structure changes

---
+++ python/leetcode/1035.uncrossed-lines.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1035 lang=python3
+#
+# [1035] Uncrossed Lines
+#
+
+# @lc code=start
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        
+# @lc code=end
+
---
+++ python/leetcode/117.populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=117 lang=python3
+#
+# [117] Populating Next Right Pointers in Each Node II
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+
+        # @lc code=end
